f59ea3280a8865286f16c224aec71f94e9a0d686
杜仁浩 <ak514250@126.com>
2021-08-15 23:11:03 +0800

    gw

---
+++ src/main/java/com/weweibuy/wfs/gw/A.java
@@ -0,0 +1,8 @@
+package com.weweibuy.wfs.gw;
+
+/**
+ * @author durenhao
+ * @date 2021/8/12 20:50
+ **/
+public class A {
+}
---
+++ src/main/java/com/weweibuy/wfs/gw/controller/GatewayQueryController.java
@@ -0,0 +1,21 @@
+package com.weweibuy.wfs.gw.controller;
+
+import com.weweibuy.wfs.gw.controller.contant.GwBasePath;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author durenhao
+ * @date 2021/8/12 20:51
+ **/
+@RestController
+@RequestMapping(GwBasePath.PATH + "/query")
+@RequiredArgsConstructor
+public class GatewayQueryController {
+
+
+
+
+
+}
